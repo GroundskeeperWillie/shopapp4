0bff56c201e87d36a85ebab1275c4b68666e3df6
Will <wmacaulay@gmail.com>
2011-03-30 14:57:56 +1100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shopapp4::Application.config.secret_token = 'e3d71d3789312f7e629671a22f6c7715cdbecd46cc78f171bc21fff30754b319ed6120e3bdf9c79bccddd701c5da2bcb3e9c817fa4aaa4fcb132935437ec3ff3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Shopapp4::Application.config.session_store :cookie_store, :key => '_shopapp4_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Shopapp4::Application.config.session_store :active_record_store
